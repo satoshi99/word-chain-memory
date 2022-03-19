24c12855dceea18a4cea2fdf36389960dc3a6d45
satoshi <3104nit@gmail.com>
2022-03-20 00:08:21 +0900

    fix props

--- components/WordCounter.tsx
+++ components/WordCounter.tsx
@@ -4,25 +4,19 @@ import {
   Flex,
   Text
 } from '@chakra-ui/react';
-import { useRecoilValue } from 'recoil';
-import { numberOfWords, wordChainList } from '../lib/recoil-atoms';
 
-export const WordCounter = () => {
-  const numWords = useRecoilValue(numberOfWords);
-  const wordList = useRecoilValue(wordChainList);
-  const count = wordList.length;
+type Props = {
+  value: number;
+  max: number;
+};
 
+export const WordCounter = ({ value, max }: Props) => {
   return (
     <Flex direction="column" position="absolute" top="5" left="5">
-      <CircularProgress
-        max={numWords}
-        value={count}
-        size="24"
-        color="teal.300"
-      />
+      <CircularProgress max={max} value={value} size="24" color="teal.300" />
       <CircularProgressLabel fontSize="2xl">
         <Text fontSize="4xl" color="teal.300">
-          {count}
+          {value}
         </Text>
       </CircularProgressLabel>
     </Flex>
